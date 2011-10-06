23c829eaeeb174a3080a5e647d8061b8b70bc599
Paris Sinclair <paris@rubypanther.com>
2011-10-05 21:15:44 -0700

    removed stale file

--- ext/dec_number/recompile.sh
+++
@@ -1,4 +0,0 @@
-rm -f dec_number.o dec_number.so
-gcc -fPIC -g -O2 -I/home/paris/.rvm/rubies/ruby-1.9.2-p290/include/ruby-1.9.1/x86_64-linux -I/home/paris/.rvm/rubies/ruby-1.9.2-p290/include/ruby-1.9.1/ -IdecNumber -c dec_number.c
-gcc -shared -o dec_number.so dec_number.o decNumber.o decContext.o -lc
-ruby -I. -e 'require "dec_number"; DecNumber.new(1.23)'
