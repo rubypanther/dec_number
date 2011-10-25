d021db29ad93d02a90b122fcf080c467b64aa0e3
Paris Sinclair <paris@rubypanther.com>
2011-10-25 14:17:26 -0700

    generate test methods

--- test/test_dec_number.rb
+++ test/test_dec_number.rb
@@ -1,9 +1,27 @@
 require 'test/unit'
 $: << File.join(File.dirname(__FILE__), '..', 'ext', 'dec_number')
 require 'dec_number'
+require 'bigdecimal'
 
 class TestDecNumber < Test::Unit::TestCase
-  def test_silly_example
-    assert_equal 1.to_dec_number, 1
+  def test_simple_equality
+    data = [ 0, 1, Math::PI, BigDecimal.new("123.5813") ]
+    data.each do |n|
+      d = n.to_dec_number
+      assert_equal d, n
+    end
   end
+
+  [ ["addition", :+ ],
+    ["subtraction", :- ]
+  ].each do |name,op|
+    define_method "test_#{name}" do
+      data = [ 0, 1, Math::PI, BigDecimal.new("123.5813") ]
+      data.each do |n|
+        d = n.to_dec_number
+        assert_equal d.send(op,d), n.send(op,n)
+      end
+    end
+  end
+
 end
