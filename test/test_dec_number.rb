fdd61a817bb6e70a8b77b2471d0ea389b743e9b2
Paris Sinclair <paris@rubypanther.com>
2011-10-25 14:18:51 -0700

    generate test methods

--- test/test_dec_number.rb
+++ test/test_dec_number.rb
@@ -4,16 +4,9 @@ require 'dec_number'
 require 'bigdecimal'
 
 class TestDecNumber < Test::Unit::TestCase
-  def test_simple_equality
-    data = [ 0, 1, Math::PI, BigDecimal.new("123.5813") ]
-    data.each do |n|
-      d = n.to_dec_number
-      assert_equal d, n
-    end
-  end
-
-  [ ["addition", :+ ],
-    ["subtraction", :- ]
+  [ [ "addition", :+ ],
+    [ "subtraction", :- ],
+    [ "equality", :== ]
   ].each do |name,op|
     define_method "test_#{name}" do
       data = [ 0, 1, Math::PI, BigDecimal.new("123.5813") ]
