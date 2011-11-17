6a1429c8fc841555d71d3bb46504f9ae01cf1e45
Paris Sinclair <paris@rubypanther.com>
2011-11-16 17:28:55 -0800

    added generated tests for when rhs is decnum and lhs is other

--- test/test_dec_number.rb
+++ test/test_dec_number.rb
@@ -12,7 +12,15 @@ class TestDecNumber < Test::Unit::TestCase
       data = [ 0, 1, Math::PI, BigDecimal.new("123.5813") ]
       data.each do |n|
         d = n.to_dec_number
-        assert_equal d.send(op,d), n.send(op,n)
+        assert_equal d.send(op,n), n.send(op,n)
+      end
+    end
+  end.each do |name,op|
+    define_method "test_#{name}_as_rval" do
+      data = [ 0, 1, Math::PI, BigDecimal.new("123.5813") ]
+      data.each do |n|
+        d = n.to_dec_number
+        assert_equal n.send(op,d), n.send(op,n)
       end
     end
   end
